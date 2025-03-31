bcfb7e9f0e1df4776d7c788940fa43973245f044
Aantoromano <aromano3@mail.austral.edu.ar>
2025-03-31 15:31:25 -0300

    Update ada.py

--- ada.py
+++ ada.py
@@ -1,3 +1,19 @@
 def ada():
     first_name = "AdA"
     last_name = "LoVeLAce"
+    
+    full_name= f"{first_name} {last_name}"  #Juntar las dos variables
+    print (full_name)
+    #AdA LoVeLAce
+
+    print (full_name.lower())
+    #ada lovelace
+
+    print (full_name.title())
+    #Ada Lovelace
+
+    print (full_name.upper())
+    #ADA LOVELACE
+
+    print (f"\t{full_name.lower()}")
+    #		ada lovelace
